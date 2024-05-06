68fd4fb6ca03f25a66033807456d462c70f435c9
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-05-06 09:41:03 +0530

    Time: 74 ms (13.76%), Space: 45.2 MB (12.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,23 +4,20 @@ class Solution {
         int left = 0;
         int right = 0;
         int maxx = 0;
-        int l = 0;
         while (right<s.length()){
             char c = s.charAt(right);
             if (!set.contains(Character.valueOf(c)) || set.isEmpty()){
                 set.add(c);
-                l++;
                 right++;
             }else{
-                maxx = Math.max(l,maxx);
+                maxx = Math.max(right-left,maxx);
                 while (s.charAt(right) != s.charAt(left)) left++;
                 right = left+1;
                 left++;
                 set.clear();
-                l = 0;
             }
         }
-        maxx = Math.max(l, maxx);
+        maxx = Math.max(right-left, maxx);
         return maxx;
     }
 }
