53b4a0424da1e8623d941cb6700f01bec5469251
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-05-11 15:03:22 +0530

    Time: 9 ms (21.12%), Space: 83 MB (17.3%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,40 +1,24 @@
+// T - O(N+N/2)
+// S - O(N)
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int pos = 0;
-        int neg = 1;
-        while (pos < nums.length && neg < nums.length){
-            if (nums[pos] < 0){
-                int t = pos;
-                int ppos = nums[pos];
-                int temp_var;
-                while (nums[t] < 0){
-                    temp_var = nums[t];
-                    nums[t] = ppos;
-                    ppos = temp_var;
-                    t++;
-                }
-                temp_var = nums[t];
-                nums[t] = ppos;
-                nums[pos] = temp_var;
+        List<Integer> pos = new ArrayList<Integer>();
+        List<Integer> neg = new ArrayList<Integer>();
+        int[] ans = new int[nums.length];
+        for (int i = 0; i<nums.length; i++){
+            if (nums[i] >=0){
+                pos.add(nums[i]);
+            }else {
+                neg.add(nums[i]);
             }
-            if (nums[neg] >= 0){
-                int t = neg;
-                int ppos = nums[neg];
-                int temp_var;
-                while (nums[t] >= 0){
-                    temp_var = nums[t];
-                    nums[t] = ppos;
-                    ppos = temp_var;
-                    t++;
-                }
-                temp_var = nums[t];
-                nums[t] = ppos;
-                nums[neg] = temp_var;
-            }
-            pos+=2;
-        neg+=2;
         }
-        return nums;
+        int counter = 0;
+        for (int i = 0; i<nums.length/2; i++){
+            ans[counter] = pos.get(i);
+            ans[counter+1] = neg.get(i);
+            counter+=2;
+        }
+        return ans;
     }
     
 }
