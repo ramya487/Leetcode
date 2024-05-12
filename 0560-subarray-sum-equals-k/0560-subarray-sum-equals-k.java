6d06d12a09bfa0a812624088219827a3b1b45954
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-05-12 15:27:20 +0530

    Time: 1516 ms (6.16%), Space: 45.9 MB (84.1%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,6 +1,9 @@
+// T - O(N) + O(N2)
+// S - O(N)
+// brute force
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int[] prefixarr = new int[nums.length];
+        int[] prefixarr = new int[nums.length]; // prefix sum array
         int summ = 0;
         for (int i = 0; i<nums.length; i++){
             summ+=nums[i];
