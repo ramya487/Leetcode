6f97fbca90d1f85e97173b411ebb1774eea20275
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-05-26 08:48:43 +0530

    Time: 1 ms (100%), Space: 43.1 MB (100%) - LeetHub

---
+++ 3446-find-the-number-of-good-pairs-i/3446-find-the-number-of-good-pairs-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int count = 0;
+        for (int i = 0; i<nums1.length; i++){
+            for (int j = 0; j<nums2.length; j++){
+                if (nums1[i]%(k*nums2[j]) == 0) count++;
+            }
+        }
+        return count;
+    }
+}
