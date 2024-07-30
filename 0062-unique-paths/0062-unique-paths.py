80182d1b3a6635a958eecde49844edafcf1b911f
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-07-30 14:57:44 +0530

    Time: 33 ms (74.21%), Space: 16.5 MB (30.36%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,15 @@
+class Solution:
+    def recur(self, i,j, dp):
+        if i == 0 and j == 0:
+            return 1
+        if i<0 or j<0:
+            return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
+        l = self.recur(i-1, j, dp)
+        r = self.recur(i,j-1, dp)
+        dp[i][j] = l+r
+        return l+r
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1 for i in range(n)] for i in range(m)]
+        return self.recur(m-1, n-1, dp)
