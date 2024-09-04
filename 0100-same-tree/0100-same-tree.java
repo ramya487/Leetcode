5596f0671f8da511e42f805658babd6afcd02f62
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-09-04 13:09:38 +0530

    Time: 0 ms (100%), Space: 40.6 MB (94.99%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,32 +16,8 @@
 // level order traversal + list comparison
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<Integer> ls1 = new ArrayList<>();
-        List<Integer> ls2 = new ArrayList<>();
-        queue.add(p);
-        while (!queue.isEmpty()){
-            TreeNode temp = queue.poll();
-            if (temp == null){
-                ls1.add(null);
-            }else{
-                ls1.add(temp.val);
-                queue.offer(temp.left);
-                queue.offer(temp.right);
-            }
-        }
-        queue.add(q);
-        while (!queue.isEmpty()){
-            TreeNode temp = queue.poll();
-            if (temp == null){
-                ls2.add(null);
-            }else{
-                ls2.add(temp.val);
-                queue.offer(temp.left);
-                queue.offer(temp.right);
-            }
-        }
-        if (ls1.equals(ls2)) return true;
-        return false;
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
     }
 }
