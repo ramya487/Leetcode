d465ec6f3b36b483e6356802d2e8616b7320fd41
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-09-22 21:07:46 +0530

    Time: 0 ms (100%), Space: 44.9 MB (8.91%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,21 +13,26 @@
  *     }
  * }
  */
-// dfs
-// T- O(N)
-// S- O(H) - recursion stack space
+// brute force
+// checking the height of left and right subtrees of all the nodes and
+// determining whether acceptable or not
+// T- O(n2)
+// S- O(h)
 class Solution {
-    public static int dfs(TreeNode root){
-        if (root == null) return 0;
-        int l = dfs(root.left);
-        if (l == -1) return -1;
-        int r = dfs(root.right);
-        if (r == -1) return -1;
-        if (Math.abs(l-r) > 1) return -1;
+    public static int depth(TreeNode node){
+        if (node == null) return 0;
+        int l = depth(node.left);
+        int r = depth(node.right);
         return Math.max(l,r)+1;
     }
 
     public boolean isBalanced(TreeNode root) {
-        return !(dfs(root) == -1);
+        if (root == null) return true;
+        int l = depth(root.left);
+        int r = depth(root.right);
+        if (Math.abs(l-r) <= 1 && isBalanced(root.left) && isBalanced(root.right)){
+            return true;
+        }
+        return false;
     }
 }
