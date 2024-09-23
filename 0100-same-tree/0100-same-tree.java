fc0e95b9024e03e54d498ab26328a22c8dced6d3
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-09-23 20:50:36 +0530

    Time: 0 ms (100%), Space: 40.7 MB (93.23%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,11 +13,11 @@
  *     }
  * }
  */
-// recursion - dfs
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        return (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        if (p == null && q != null || q == null && p != null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
