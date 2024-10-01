e2a15d0577a475a7b6405da910f4de703b251c68
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-10-01 21:23:38 +0530

    Time: 0 ms (100%), Space: 41.8 MB (90.65%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,16 +13,19 @@
  *     }
  * }
  */
+ // T- O(N)
+ // S- O(H)
+ // recursion to avoid the complexity of level order traversal
 class Solution {
-    public static List<Integer> recur(TreeNode root, int level, List<Integer> ds){
-        if (root == null) return null;
+    public static void recur(TreeNode root, int level, List<Integer> ds){
+        if (root == null) return;
         if (ds.size() == level) ds.add(root.val);
         recur(root.right, level+1, ds);
         recur(root.left, level+1, ds);
-        return ds;
     }
     public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) return new ArrayList<Integer>();
-        return recur(root, 0, new ArrayList<Integer>());
+        List<Integer> ds = new ArrayList<Integer>();
+        recur(root, 0, ds);
+        return ds;
     }
 }
