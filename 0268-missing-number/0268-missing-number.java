089f85dbb46b39e9c0a97a2ebb98b41f4c9903be
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-11-01 11:52:37 +0530

    Time: 0 ms (100%), Space: 45.3 MB (31.92%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int tsum = n*(n+1)/2;
+        int osum = 0;
+        for (int i = 0; i<n; i++){
+            osum+=nums[i];
+        }
+        return tsum-osum;
+    }
+}
