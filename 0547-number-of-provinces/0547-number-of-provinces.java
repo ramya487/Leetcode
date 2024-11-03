193c6f17f7de333b1b4de3f605455f7b13452beb
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-11-03 09:25:19 +0530

    Time: 3 ms (39.89%), Space: 47.2 MB (69.33%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static List<List<Integer>> createAdjList(int[][] matrix){
+        int n = matrix.length;
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i<n; i++){
+            adjList.add(new ArrayList<>());
+        }
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                if (matrix[i][j] == 1) adjList.get(i).add(j);
+            }
+        }
+
+        return adjList;
+    }
+
+    public static void dfs(List<List<Integer>> adjList, int[] vis, Integer node){
+        vis[node] = 1;
+        for (Integer neighbour: adjList.get(node)){
+            if (vis[neighbour] == 0) dfs(adjList, vis, neighbour);
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        List<List<Integer>> adjList = createAdjList(isConnected);
+        int provinces = 0;
+        int n = isConnected.length;
+        int[] vis = new int[n];
+        for (int i = 0; i<n; i++){
+            if (vis[i] == 0){
+                provinces++;
+                dfs(adjList, vis, i);
+            }
+        }
+        return provinces;
+    }
+}
