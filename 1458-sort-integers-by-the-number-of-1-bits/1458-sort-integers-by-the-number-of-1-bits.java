478687d384815cc654c200f7e4df8d591c95b6a8
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-11-04 10:25:09 +0530

    Time: 7 ms (70.79%), Space: 45 MB (15.08%) - LeetHub

---
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static int noOfSetBit(int n){
+        int count = 0;
+        while (n>1){
+            if (n%2 == 1) count++;
+            n = n/2;
+        }
+        if (n == 1) count++;
+        return count;
+    }
+    public int[] sortByBits(int[] arr) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> {
+            if (arr1[1] != arr2[1]) return Integer.compare(arr1[1], arr2[1]);
+            return Integer.compare(arr1[0], arr2[0]);
+        });
+        
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int c = noOfSetBit(arr[i]);
+            pq.add(new int[]{arr[i], c});
+        }
+        
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            ret[i] = pq.poll()[0];
+        }
+        
+        return ret;
+    }
+}
