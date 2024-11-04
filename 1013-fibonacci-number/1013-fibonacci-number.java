4105f16c74fb67096fdf95816ad80c4c68ad2c3e
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-11-04 09:52:21 +0530

    Time: 0 ms (100%), Space: 40.6 MB (18.89%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,13 +1,17 @@
+// T- O(N)
+// S- O(N)
 class Solution {
     public int fib(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
+        int first = 0;
+        int next = 1;
+        int req = first+next;
         for (int i = 2; i<=n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            req = first+next;
+            first = next;
+            next = req;
         }
-        return dp[n];
+        return req;
     }
 }
