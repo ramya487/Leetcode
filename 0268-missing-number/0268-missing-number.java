e3d8eef395cd008f77050ef2a904cef1babb5983
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-11-20 13:06:57 +0530

    Time: 0 ms (100%), Space: 44.7 MB (98.07%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,16 @@
-// math and logic
-//T- O(N)
+// xor
+//T- O(2N)
 //S- O(1)
 class Solution {
     public int missingNumber(int[] nums) {
+        int xor = 0;
         int n = nums.length;
-        int tsum = n*(n+1)/2;
-        int osum = 0;
-        for (int i = 0; i<n; i++){
-            osum+=nums[i];
+        for (int i = 0; i<=n; i++){
+            xor = xor^i;
         }
-        return tsum-osum;
+        for (int i = 0; i<n; i++){
+            xor = xor^nums[i];
+        }
+        return xor;
     }
 }
