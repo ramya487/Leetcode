e42837ffa20945d252df809164978d58699c5cb7
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-11-27 18:18:10 +0530

    Time: 1 ms (45.36%), Space: 45 MB (36.51%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,49 +1,26 @@
-// bfs
-
-class Pair {
-    int r,c;
-    Pair(int r, int c){
-        this.r = r;
-        this.c = c;
-    }
-}
-
+// T- O(n*m)
+// S- O(n*m)
+// dfs - matrix
 class Solution {
+    public static void dfs(int r, int c, int[][] image, int[][] ret, int color, int prevcolor, int[] delrow, int[] delcol){
+        ret[r][c] = color;
+        for (int i = 0; i<4; i++){
+            int nr = r+delrow[i];
+            int nc = c+delcol[i];
+            if (nr>=0 && nc>=0 && nr < image.length && nc < image[0].length && image[nr][nc] == prevcolor && ret[nr][nc] != color){
+                dfs(nr,nc,image,ret, color, prevcolor, delrow, delcol);
+            }
+        }
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        int[][] ret = new int[m][n];
-        for (int i = 0; i<m; i++){
-            for (int j = 0; j<n; j++){
-                ret[i][j] = image[i][j];
-            }
-        }
-
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.offer(new Pair(sr,sc));
-        int val = image[sr][sc];
-
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0,1,0,-1};
-
-        int[][] vis = new int[m][n];
-        vis[sr][sc] = 1;
-
-        while (!q.isEmpty()){
-            Pair frnt = q.poll();
-            int r = frnt.r;
-            int c = frnt.c;
-            ret[r][c] = color;
-            vis[r][c] = 1;
-            for (int i = 0; i<4; i++){
-                int row = r+delRow[i];
-                int col = c+delCol[i];
-                if (row>=0 && col>=0 && row<m && col<n && ret[row][col] == val && vis[row][col] != 1){
-                    q.offer(new Pair(row, col));
-                }
-            }
-        }
-
+        int r = image.length;
+        int c = image[0].length;
+        int[][] ret = image;
+        // System.out.println(Arrays.deepToString(ret));
+        int prevcolor = image[sr][sc];
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
+        dfs(sr,sc,image,ret, color, prevcolor, delrow, delcol);
         return ret;
     }
 }
