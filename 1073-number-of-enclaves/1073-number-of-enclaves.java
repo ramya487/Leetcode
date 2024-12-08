56d25c063fa282e95c8235bba8f5ecad2b6b9751
Ramya S <124439274+ramya487@users.noreply.github.com>
2024-12-08 13:34:03 +0530

    Time: 8 ms (78.11%), Space: 53.7 MB (83.99%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,42 +1,49 @@
+// graph - adj.mat - bfs
+class Tuple{
+    int i,j;
+    Tuple(int i, int j){
+        this.i = i;
+        this.j = j;
+    }
+}
 class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] visited = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            visited[i] = grid[i].clone();
-        }
-        Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i == 0 || i == m-1 || j == 0 || j == n-1) && (visited[i][j] == 1)) {
-                    q.offer(new int[]{i, j});
-                    visited[i][j] = 0;
+        int[][] vis = new int[m][n];
+
+        int[] delRow = {-1,0,1,0};
+        int[] delCol = {0,1,0,-1};
+
+        Queue<Tuple> q = new LinkedList<>();
+        for (int i = 0; i<m; i++){
+            for (int j = 0; j<n; j++){
+                if (i == 0 || j == 0 || i == m-1 || j == n-1){
+                    if (grid[i][j] == 1){
+                        q.offer(new Tuple(i,j));
+                        vis[i][j] = 1;
+                    }
                 }
             }
         }
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int x = cell[0];
-            int y = cell[1];
-            for (int[] dir : dirs) {
-                int i = x + dir[0];
-                int j = y + dir[1];
-                if (i >= 0 && j >= 0 && i < m && j < n && visited[i][j] == 1) {
-                    q.offer(new int[]{i, j});
-                    visited[i][j] = 0;
+        while (!q.isEmpty()){
+            Tuple node = q.poll();
+            for (int counter = 0; counter<4; counter++){
+                int nr = node.i+delRow[counter];
+                int nc = node.j+delCol[counter];
+                if (nr>=0 && nr<vis.length && nc>=0 && nc<vis[0].length && vis[nr][nc] == 0 && grid[nr][nc] == 1){
+                    q.offer(new Tuple(nr, nc));
+                    vis[nr][nc] = 1;
                 }
             }
         }
-        int countLands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 1) {
-                    countLands++;
-                }
+
+        int landsNotVisited = 0;
+        for (int i = 0; i<m; i++){
+            for (int j = 0; j<n; j++){
+                if (grid[i][j] == 1 && vis[i][j] == 0) landsNotVisited++;
             }
         }
-        return countLands;
+        return landsNotVisited;
     }
 }
