0b060d08001c84f90c1525a1c0887df0678dcd96
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-01-05 19:14:05 +0530

    Time: 10 ms (14.59%), Space: 82.9 MB (16.76%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,24 +1,28 @@
-// T - O(N+N/2)
-// S - O(N)
+// T- O(2n)
+// S- O(2n)
+
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> pos = new ArrayList<Integer>();
-        List<Integer> neg = new ArrayList<Integer>();
-        int[] ans = new int[nums.length];
-        for (int i = 0; i<nums.length; i++){
-            if (nums[i] >=0){
-                pos.add(nums[i]);
-            }else {
-                neg.add(nums[i]);
+        int n = nums.length;
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
+        int[] ret = new int[n];
+        for (int i = 0; i<n; i++){
+            int ele = nums[i];
+            if (ele>0) pos.add(ele);
+            else neg.add(ele);
+        }
+        int p = 0;
+        int ng = 0;
+        for (int i = 0; i<n; i++){
+            if (i%2 == 0){
+                ret[i] = pos.get(p);
+                p++;
+            }else{
+                ret[i] = neg.get(ng);
+                ng++;
             }
         }
-        int counter = 0;
-        for (int i = 0; i<nums.length/2; i++){
-            ans[counter] = pos.get(i);
-            ans[counter+1] = neg.get(i);
-            counter+=2;
-        }
-        return ans;
+        return ret;
     }
-    
 }
