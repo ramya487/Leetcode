92bea49c3947dd9c16cbbee741f5063c6fb7df9a
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-01-12 12:39:27 +0530

    Time: 0 ms (100%), Space: 42 MB (52.6%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,24 +1,21 @@
+// T- O(n2)
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> initlst = new ArrayList<List<Integer>>();
-        List<Integer> temp = new ArrayList<Integer>();
-        temp.add(1);
-        initlst.add(temp);
-        
-        System.out.println(initlst);
-
-        for (int i = 1; i<numRows; i++){
-            List<Integer> l = new ArrayList<Integer>();
-            List<Integer> lst = initlst.get(i-1);
-            for (int j = 0; j<=i; j++){
-                if (j-1>=0 && j<lst.size()){
-                    l.add(lst.get(j-1)+lst.get(j));
-                }else {
-                    l.add(1);
-                }
-            }
-            initlst.add(l);
+    public static List<Integer> generateRow(int n){
+        int ans = 1;
+        List<Integer> ls = new ArrayList<>();
+        ls.add(ans);
+        for (int i = 1; i<n; i++){
+            ans = ans*(n-i);
+            ans = ans/i;
+            ls.add(ans);
         }
-        return initlst;
+        return ls;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 1; i<=numRows; i++){
+            ret.add(generateRow(i));
+        }
+        return ret;
     }
 }
