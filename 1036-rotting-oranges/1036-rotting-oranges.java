184c6c6d02c856edefc4682139c03275567427a0
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-02-07 12:42:18 +0530

    Time: 2 ms (70.95%), Space: 42.6 MB (38.28%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,55 +1,56 @@
-// bfs on 2d matrix
-
-class Pair{
-    int r,c,tm;
-    Pair(int r, int c, int tm){
-        this.r = r;
-        this.c = c;
-        this.tm = tm;
+class Pair {
+    int i,j;
+    Pair(int i, int j){
+        this.i = i;
+        this.j = j;
     }
 }
 
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<Pair> q = new LinkedList<>();
         int m = grid.length;
         int n = grid[0].length;
-        int[][] vis = new int[m][n];
-        Queue<Pair> q = new LinkedList<>();
-        int freshCount = 0;
+        int fresh = 0;
         for (int i = 0; i<m; i++){
             for (int j = 0; j<n; j++){
                 if (grid[i][j] == 2){
-                    q.offer(new Pair(i,j,0));
-                    vis[i][j] = 2;
+                    q.offer(new Pair(i,j));
                 }
-                else if (grid[i][j] == 1) freshCount++;
+                if (grid[i][j] != 0){
+                    fresh++;
+                }
             }
         }
 
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
+        if (fresh == 0) return 0;
+        if (q.isEmpty()) return -1;
 
+        int minn = -1;
+        int visf = 0;
         int cnt = 0;
-        int maxMin = 0;
+
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
 
         while (!q.isEmpty()){
-            Pair frnt = q.poll();
-            int r = frnt.r;
-            int c = frnt.c;
-            int tm = frnt.tm;
-            maxMin = Math.max(maxMin, tm);
-            for (int i = 0; i<4; i++){
-                int row = r+delRow[i];
-                int col = c+delCol[i];
-                if (row>=0 && col>=0 && row<m && col<n && vis[row][col] == 0 && grid[row][col] == 1){
-                    q.offer(new Pair(row, col, tm+1));
-                    cnt++;
-                    vis[row][col] = 2;
+            int size = q.size();
+            minn++;
+            cnt+=size;
+            for (int counter = 0; counter<size; counter++){
+                Pair cell = q.poll();
+                int i = cell.i;
+                int j = cell.j;
+                for (int c = 0; c<4; c++){
+                    int nr = delrow[c]+i;
+                    int nc = delcol[c]+j;
+                    if (nr<0 || nc < 0 || nr>= m || nc >= n || grid[nr][nc] == 0 || grid[nr][nc] == 2) continue;
+                    grid[nr][nc] = 2;
+                    q.offer(new Pair(nr, nc));
                 }
             }
         }
 
-        if (freshCount != cnt) return -1;
-        return maxMin;
+        return (fresh == cnt) ? minn : -1;
     }
 }
