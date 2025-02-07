6e623b519537a504daa184c60cd7d89afba5b7ab
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-02-07 10:41:02 +0530

    Time: 1 ms (88.45%), Space: 48.4 MB (13.93%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,23 +1,28 @@
 class Solution {
-    public static void dfs(int[][] matrix, int[] vis, int node){
-        vis[node] = 1;
-        for (int i = 0; i<matrix.length; i++){
-            if (vis[i] == 0 && matrix[node][i] == 1){
-                dfs(matrix, vis, i);
+    public void BFS(int u, int[][] isConnected, boolean[] visited){
+        visited[u] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(u);
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            for(int i = 0; i< isConnected.length; i++){
+                if(!visited[i] && isConnected[curr][i] == 1){
+                    visited[i] = true;
+                    queue.offer(i);
+                }
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int[] vis = new int[n];
-        int p = 0;
-        for (int i = 0; i<n; i++){
-            if (vis[i] == 0){
-                p++;
-                dfs(isConnected, vis, i);
+        boolean[] visited = new boolean[n+1];
+        int count = 0;
+        for(int i = 0; i< n; i++){
+            if(!visited[i]){
+                count++;
+                BFS(i,isConnected,visited);
             }
         }
-
-        return p;
+        return count;
     }
 }
