7dbb39e7b48f95b34b26d91f9fc97fe3c0e89176
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-02-07 13:06:08 +0530

    Time: 0 ms (100%), Space: 45.3 MB (10.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,23 @@
-// T- O(n*m)
-// S- O(n*m)
-// dfs - matrix
 class Solution {
-    public static void dfs(int r, int c, int[][] image, int[][] ret, int color, int prevcolor, int[] delrow, int[] delcol){
-        ret[r][c] = color;
-        for (int i = 0; i<4; i++){
-            int nr = r+delrow[i];
-            int nc = c+delcol[i];
-            if (nr>=0 && nc>=0 && nr < image.length && nc < image[0].length && image[nr][nc] == prevcolor && ret[nr][nc] != color){
-                dfs(nr,nc,image,ret, color, prevcolor, delrow, delcol);
+    public static void dfs(int[][] image, int i, int j, int origColor, int newColor, int[][] vis){
+        if (image[i][j] != origColor || vis[i][j] != 0) return;
+        image[i][j] = newColor;
+        vis[i][j] = 1;
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
+        for (int counter = 0; counter<4; counter++){
+            int nr = delrow[counter]+i;
+            int nc = delcol[counter]+j;
+            if (nr>=0 && nc>=0 && nr<image.length && nc<image[0].length){
+                dfs(image, nr, nc, origColor, newColor, vis);
             }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int r = image.length;
-        int c = image[0].length;
-        int[][] ret = image;
-        // System.out.println(Arrays.deepToString(ret));
-        int prevcolor = image[sr][sc];
-        int[] delrow = {-1,0,1,0};
-        int[] delcol = {0,1,0,-1};
-        dfs(sr,sc,image,ret, color, prevcolor, delrow, delcol);
-        return ret;
+        int m = image.length;
+        int n = image[0].length;
+        int[][] vis = new int[m][n];
+        dfs(image, sr, sc, image[sr][sc], color, vis);
+        return image;
     }
 }
