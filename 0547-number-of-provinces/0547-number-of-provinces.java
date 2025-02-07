95d9c6abd0e28991a74892b01423b53033f0865f
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-02-07 10:18:19 +0530

    Time: 2 ms (46.84%), Space: 47.3 MB (77.71%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,39 +1,36 @@
-// T- O(N2) + O(N) + 
-
 class Solution {
-    public static List<List<Integer>> createAdjList(int[][] matrix){
-        int n = matrix.length;
+    public static void dfs(List<List<Integer>> adjList, int[] vis, int node){
+        vis[node] = 1;
+        for (int neighbour: adjList.get(node)){
+            if (vis[neighbour] == 0){
+                dfs(adjList, vis, neighbour);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        
         List<List<Integer>> adjList = new ArrayList<>();
         for (int i = 0; i<n; i++){
             adjList.add(new ArrayList<>());
         }
         for (int i = 0; i<n; i++){
             for (int j = 0; j<n; j++){
-                if (matrix[i][j] == 1 && j != i) adjList.get(i).add(j);
+                if (isConnected[i][j] == 1 && j != i){
+                    adjList.get(i).add(j);
+                }
             }
         }
 
-        return adjList;
-    }
-
-    public static void dfs(List<List<Integer>> adjList, int[] vis, Integer node){
-        vis[node] = 1;
-        for (Integer neighbour: adjList.get(node)){
-            if (vis[neighbour] == 0) dfs(adjList, vis, neighbour);
-        }
-    }
-
-    public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> adjList = createAdjList(isConnected);
-        int provinces = 0;
-        int n = isConnected.length;
         int[] vis = new int[n];
+        int p = 0;
         for (int i = 0; i<n; i++){
             if (vis[i] == 0){
-                provinces++;
+                p++;
                 dfs(adjList, vis, i);
             }
         }
-        return provinces;
+
+        return p;
     }
 }
