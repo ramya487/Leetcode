78dc5dca49610f0742389bbdcaca0dc758051059
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-02-10 12:37:48 +0530

    Time: 14 ms (59.66%), Space: 47.2 MB (71.04%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,31 +1,54 @@
+class Node {
+    int i, j, d;
+
+    Node(int i, int j, int d) {
+        this.i = i;
+        this.j = j;
+        this.d = d;
+
+    }
+}
+
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        int[][] arr = new int[m][n];
-        
-        // Initialize arr with large values for cells containing 1
+
+        int[][] vis = new int[m][n];
+        int[][] dist = new int[m][n];
+
+        Queue<Node> q = new LinkedList<>();
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                arr[i][j] = mat[i][j] == 0 ? 0 : Integer.MAX_VALUE - 10000; // Avoid integer overflow
-            }
-        }
-        
-        // Iterate through each cell and find the minimum distance to a 0
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) {
-                    for (int x = 0; x < m; x++) {
-                        for (int y = 0; y < n; y++) {
-                            if (mat[x][y] == 0) {
-                                arr[i][j] = Math.min(arr[i][j], Math.abs(x - i) + Math.abs(y - j));
-                            }
-                        }
-                    }
+                if (mat[i][j] == 0) {
+                    q.offer(new Node(i, j, 0));
+                    vis[i][j] = 1;
                 }
             }
         }
-        
-        return arr;
+
+        int[] delrow = { -1, 0, 1, 0 };
+        int[] delcol = { 0, 1, 0, -1 };
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int r = node.i;
+            int c = node.j;
+            int dt = node.d;
+            dist[r][c] = dt;
+
+            for (int counter = 0; counter < 4; counter++) {
+                int nr = r + delrow[counter];
+                int nc = c + delcol[counter];
+
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && vis[nr][nc] == 0 && mat[nr][nc] == 1) {
+                    q.offer(new Node(nr, nc, dt + 1));
+                    vis[nr][nc] = 1;
+                }
+            }
+        }
+
+        return dist;
     }
 }
