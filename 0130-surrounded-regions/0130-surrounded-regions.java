f7ff60e01949c29b9d3b31c59a506b99c3606993
Ramya S <124439274+ramya487@users.noreply.github.com>
2025-02-14 09:58:58 +0530

    Time: 2 ms (86.15%), Space: 45.9 MB (15.39%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,13 +1,16 @@
-// graph - dfs
 class Solution {
-    public static void dfs(int i, int j, int[] delrow, int[] delcol, int[][] vis, char[][] mat){
+    public static void dfs(int i, int j, char[][] board, int[][] vis){
+        if (vis[i][j] == 1) return;
         vis[i][j] = 1;
+        int[] delrow = {-1, 0, 1, 0};
+        int[] delcol = {0,1,0,-1};
+        int m = board.length;
+        int n = board[0].length;
         for (int counter = 0; counter<4; counter++){
-            int nr = i+delrow[counter];
-            int nc = j+delcol[counter];
-            if (nr>=0 && nr<vis.length && nc>=0 && nc<vis[0].length && mat[nr][nc] == 'O' && vis[nr][nc] != 1){
-                dfs(nr, nc, delrow, delcol, vis, mat);
-            }
+            int nr = i + delrow[counter];
+            int nc = j + delcol[counter];
+            if (nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc] == 'O')
+                dfs(nr, nc, board, vis);
         }
     }
     public void solve(char[][] board) {
@@ -15,21 +18,29 @@ class Solution {
         int n = board[0].length;
 
         int[][] vis = new int[m][n];
-        int[] delrow = {-1,0,1,0};
-        int[] delcol = {0,1,0,-1};
 
-        for (int i = 0; i<m; i++){
-            if (board[i][0] == 'O') dfs(i, 0, delrow, delcol, vis, board);
-            if (board[i][n-1] == 'O') dfs(i, n-1, delrow, delcol, vis, board);
+        for (int i = 0; i<n; i++){
+            if (board[0][i] == 'O')
+                dfs(0, i, board, vis);
         }
         for (int i = 0; i<n; i++){
-            if (board[0][i] == 'O') dfs(0, i, delrow, delcol, vis, board);
-            if (board[m-1][i] == 'O') dfs(m-1, i, delrow, delcol, vis, board);
+            if (board[m-1][i] == 'O')
+                dfs(m-1, i, board, vis);
         }
-        System.out.println(Arrays.deepToString(vis));
+        for (int i = 0; i<m; i++){
+            if (board[i][0] == 'O')
+                dfs(i, 0, board, vis);
+        }
+        for (int i = 0; i<m; i++){
+            if (board[i][n-1] == 'O')
+                dfs(i, n-1, board, vis);
+        }
+
         for (int i = 0; i<m; i++){
             for (int j = 0; j<n; j++){
-                if (vis[i][j] != 1) board[i][j] = 'X';
+                if (vis[i][j] == 0 && board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
             }
         }
     }
